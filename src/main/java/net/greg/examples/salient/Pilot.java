aabac8307fca5106dbd19e6d7f27d23e2e16201b
gregallentrester <gregallentrester@gmail.com>
2023-03-03 03:38:04 -0600

    Add files via upload

---
+++ src/main/java/net/greg/examples/salient/Pilot.java
@@ -0,0 +1,15 @@
+package net.greg.examples.salient;
+
+abstract class Pilot {
+  abstract num (int a, int b) {  }
+      public static void main(String[] args) {
+         Pilot obj = new Pilot(n);
+ }
+ static int a = 10;
+ static int n;
+ int b = 5;
+ int c;
+ public Pilot(int m) {
+       System.out.println(a + ", " + b + ", " + c + ", " + n + ", " + m);
+ }
+}
